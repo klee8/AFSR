29105c21a779d2d4e25f8bf572e2c01886f60608
klee8 <klee555@aucklanduni.ac.nz>
2017-04-01 13:00:46 +1300

    changed 'other_sb' output to 'unidentified' and changed to allow the input files to be names

--- sum_labelfiles.pl
+++ sum_labelfiles.pl
@@ -4,28 +4,29 @@
 # Takes in a number of label files and summarises output (to reduce false positives) 
 # i.e. only want to identify a bird when the model for that bird is positive and all other models indicate "other_sb"
 
-# input: text file called (sum_me.txt) with a list of the files to be summarised (one on each line), categories.txt file with a list of all the categories
-# usage: perl sum_labelfiles.pl 
+# input: text file (default sum_me.txt) with a list of the files to be summarised (one on each line), categories.txt file with a list of all the categories
+# usage: perl sum_labelfiles.pl <sum_me.txt> <outfile_name>
 
 
 ######################################################################################################################################
 
 # RULES (from Ivan):
-# 1. When the five labels are indicating the same category, maintain that category
+# 1. When the five labels are indicating the same category, use that category
 #       -(example1: five models indicating “background”)
 #       -(example2:  five models indicating “other_sb”)
-# 2. The  “background” should be maintained every time it appear in at least one of the five models, with two exceptions:
-#       -Exception 1:  when “background” appears at the same time as “noise” – maintain “noise” in this case.
-#       -Exception 2:  when “background” appears at the same time as two different bird species – maintain “other_sb” in this case.
-# 3. The “noise” should be maintained every time it appear in one of the five models.
-# 4. When among the five models there are 2 “categories” indicated and of the categories is a bird species and the other is “other_sb”,
-#   maintain the bird species (ex: one model indicate GFP and all the others  indicate “other_sb” = GFP should be indicated)
-# 5. When among the five models there are 3 “categories” indicated:
-#        5A: In that case should be maintained the category “other_sb”. 
-#            (example: two of the categories are  bird species and the other is “other_sb” or “background”).
-#            In this case the “other_sb” should be indicated from the beginning of the bird species which started first and go until 
+# 2. The  “background” label should be used every time it appears in at least one of the five models, with two exceptions:
+#       -Exception 1:  when “background” appears at the same time as “noise” – use “noise” in this case.
+#       -Exception 2:  when “background” appears at the same time as two different bird species – use “unidentified” in this case.
+# 3. The “noise” label should be used every time it appear in one of the five models.
+# 4. When among the five models there are 2 categories indicated and one of the categories is a bird species and the other is “other_sb”,
+#   use the bird species label (ex: one model indicate GFP and all the others indicate “other_sb” = the GFP label should be used)
+# 5. When among the five models there are 3 categories indicated:
+#        5A: In that case the category "unidentified” should be used. 
+#            (example: two of the categories are bird species and the other is “other_sb” or “background”).
+#            In this case the “unidentified” should be indicated from the beginning of the bird species which started first and go until 
 #            the end of the second species.
-# 6. When among the five models there are 4 ”categories” maintain “other_sb”.
+# 6. When among the five models there are 4 categories use “unidentified”.
+# 7. Replace "other_sb" label with "unidentified"
 
 ####################################################################################################################################### 
 
@@ -33,14 +34,24 @@
 use strict; 
 use warnings; 
 
+my $sumfile;
+my $outfile;
+
+
+if ( (scalar(@ARGV) != 0) && (scalar(@ARGV) != 2) ) { die "ERROR: input must be 'perl sum_labelfiles.pl' OR 'perl sum_labelfiles.pl <sum_file.txt> <outfile_name> ";}
+
+
+if (@ARGV == 0) {$sumfile = "sum_me.txt"; $outfile = "summary.labels.txt"}
+else {$sumfile = $ARGV[0]; $outfile = $ARGV[1]}
+
 # open log file
-open (LOG, ">log.txt") || die "ERROR, couldn't open log file: $!";
+open (LOG, "> label.log") || die "ERROR, couldn't open log file: $!";
 
 # open list of files 
-open (SUMME, "<sum_me.txt") || die "ERROR, couldn't open sum_me.txt: $!";
+open (SUMME, "< $sumfile") || die "ERROR, couldn't open sum_me.txt: $!";
 
 # open output file
-open (OUT, ">summary.label") || die "ERROR, couldn't open output file: $!";
+open (OUT, "> $outfile") || die "ERROR, couldn't open output file: $!";
 
 # read in files in sum_me.txt list and put each into an array-of-arrays (@dataset)
 my @dataset;
@@ -85,6 +96,8 @@ while(<CAT>){
     print LOG "$_\n";
 }
 
+print LOG "\n\n\n";
+
 # species list hash
 my %species = %categories;
 delete($species{'background'});
@@ -117,22 +130,22 @@ until ($current_end == $endtime){
 	$categories{$key} = 0;
 	#print "$key\t";
     }
-    print "\n";
+    print LOG "\n";
 
     # move to next block, skip values = 0
     for ($i=0; $i <= $filecounter -1; $i++){
 	my @values = split /\t/, $dataset[$i][0];
 	if ($values[1] == $checkpoint) { shift(@{$dataset[$i]}); }
 	@values = split /\t/, $dataset[$i][0];
-	if ($values[2] eq "0") { print "0 was here\n";shift(@{$dataset[$i]}); }
+	if ($values[2] eq "0") { print LOG "0 was here\n";shift(@{$dataset[$i]}); }
     }
 
-    print "______________________________________________________________________________________________\n"; 
+    print LOG "______________________________________________________________________________________________\n"; 
     # get values for all models in next time block and mark the smallest end time as the next checkpoint
     my %section;
     $checkpoint = $checkpoint + 100;
     for ($i=0; $i <= $filecounter -1; $i++){
-	print "$i $dataset[$i][0] \n";
+	print LOG "$i $dataset[$i][0] \n";
 	my @values = split ("\t", $dataset[$i][0]);
 	$section{category}{$i} = $values[2];
 	$section{start}{$i} = $values[0];
@@ -163,11 +176,11 @@ until ($current_end == $endtime){
 	for my $i (sort keys %species){                                            # check for birds identified
 	    if ($categories{$i} > 0) { $birdcount++; $tempcategory = $i; }
 	}
-	if ( ($manybirds == 0) && ($birdcount > 1) ) { $manybirds = 1; $current_category = 'other_sb'; }    # no 'noise', manybirds = 0, 2+ birds  ->  'other_sb', change manybirds = 1
-	elsif ( ($manybirds == 1) && ($birdcount > 1) ) { $current_category = 'other_sb'; }                 # no 'noise', manybirds = 1, 2+ birds  ->  'other_sb'  
+	if ( ($manybirds == 0) && ($birdcount > 1) ) { $manybirds = 1; $current_category = 'unidentified'; }    # no 'noise', manybirds = 0, 2+ birds  ->  'unidentified', change manybirds = 1
+	elsif ( ($manybirds == 1) && ($birdcount > 1) ) { $current_category = 'unidentified'; }                 # no 'noise', manybirds = 1, 2+ birds  ->  'unidentified'  
 	elsif ( ( $manybirds == 1) && ($birdcount == 1) ) { 
 	    if ($categories{'background'} > 0) {$current_category = 'background'; $manybirds = 0; }  # no 'noise', manybirds flag = 1, one bird, 'background' > 0 -> 'background', change manybirds = 0, 
- 	    else { $current_category = 'other_sb';}                                # no 'noise', manybirds flag = 1, one bird, no 'background'   ->  'other_sb', 
+ 	    else { $current_category = 'unidentified';}                                # no 'noise', manybirds flag = 1, one bird, no 'background'   ->  'unidentified', 
 	}
 	elsif ( ( $manybirds == 0) && ($birdcount == 1) ) { 
 	    if ($categories{'background'} > 0) {$current_category = 'background';} # no 'noise', manybirds = 0, one bird, 'background' > 0  -> 'background'
@@ -175,12 +188,12 @@ until ($current_end == $endtime){
 	}
 	elsif ($birdcount == 0){
 	    if ($categories{'background'} > 0) { $current_category = 'background'; $manybirds = 0;}    # no 'noise', no birds, 'background' > 0   -> 'background', change manybirds = 0
-	    elsif ($categories{'other_sb'} > 0) { $current_category = 'other_sb'; $manybirds = 0;}     # no 'noise', no birds, no 'background', 'other_sb' > 0  -> 'other_sb', change manybirds = 0
+	    elsif ($categories{'other_sb'} > 0) { $current_category = 'unidentified'; $manybirds = 0;}     # no 'noise', no birds, no 'background', 'other_sb' > 0  -> 'unidentified', change manybirds = 0
       	}
 	else { $current_category = 'unknown'; }
     }
     #elsif ($categories{'background'} > 0) { $current_category = 'background'; $manybirds = 0; }   
-    #elsif ($categories{'other_sb'} > 0) { $current_category = 'other_sb' ; }
+    #elsif ($categories{'other_sb'} > 0) { $current_category = 'unidentified' ; }
     else {$current_category = 'unknown';}
 
     #print "\ncurrent category = $current_category\n";
@@ -203,12 +216,12 @@ until ($current_end == $endtime){
 	if ($manybirds == 1) { $current_start = $last_start;} 
 	else { $current_start = $last_end; } 
 	unless ( ($last_end == $starttime) ||  ( ($manybirds == 1) && ($last_category ne 'background') && ($last_category ne 'noise') ) ){
-	    print "\n******SUM LINE: $last_start\t$last_end\t$last_category*********\n";
+	    print LOG "\n******SUM LINE: $last_start\t$last_end\t$last_category*********\n";
 	    print OUT "$last_start\t$last_end\t$last_category\n";
 	}
     }
     
-    print "current category = $current_category\ncurrent start = $current_start\ncurrent end = $current_end\ncurrent manybirds = $manybirds\nbirdcount = $birdcount\n";	
+    print LOG "current category = $current_category\ncurrent start = $current_start\ncurrent end = $current_end\ncurrent manybirds = $manybirds\nbirdcount = $birdcount\n";	
 
 }
 
